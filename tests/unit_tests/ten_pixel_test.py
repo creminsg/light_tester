ea1b88e2251a50e4a2ae22af429dca05ae50d590
creminsg <gary.cremins1@ucdconnect.ie>
2018-03-03 22:43:09 +0000

    added test to check that invalid commands will be ignored

--- tests/unit_tests/ten_pixel_test.py
+++ tests/unit_tests/ten_pixel_test.py
@@ -5,8 +5,6 @@ from light_tester import *
 import pytest
 from click.testing import CliRunner
 
-
-
 def testParseOnlineFile():
 	file = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
 	N, instructions = light_tester.LightTester.parseFile(file)
@@ -18,3 +16,11 @@ def testParseLocalFile():
         N, instructions = light_tester.LightTester.parseFile(file)
         assert N is not None
         assert instructions is not None
+
+def testIgnoreInvalidCommands():
+        N, instructions = light_tester.LightTester.parseFile("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
+        totalCommandCount = 0
+        validCommandCount = light_tester.LightTester.apply(instructions)
+        for i in instructions:
+                totalCommandCount += 1
+        assert validCommandCount != totalCommandCount

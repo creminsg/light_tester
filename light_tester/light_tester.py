096adf3820694081814b888934f319c2cf9dd696
creminsg <gary.cremins1@ucdconnect.ie>
2018-03-03 13:06:36 +0000

    changed pathfile for local file test and adding main module light_tester

--- light_tester/light_tester.py
+++ light_tester/light_tester.py
@@ -1,3 +1,34 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import urllib.request
+import re 
+
+class LightTester:
+    
+    def __init__(self, N):
+        N = int(N)
+        self.lights = [[False]*N for i in range(N)] 
+    
+    def parseFile(A):
+        instructions = []
+        if A.startswith('http'):
+            response = urllib.request.urlopen(A)
+            data = response.read().decode('utf-8')
+            z = re.split('\n', data)
+            for i in z:
+                instructions.append(i)
+            N = instructions.pop(0)
+            return N, instructions
+
+        else:
+            N, instructions = None, []
+            with open(A,'r') as f:
+                N = int(f.readline()) 
+                for line in f.readline(): 
+                        instructions.append(line)
+                return N, instructions
+        return 
+
+
--- tests/unit_tests/ten_pixel_test.py
+++ tests/unit_tests/ten_pixel_test.py
@@ -14,23 +14,7 @@ def testParseOnlineFile():
 	assert instructions is not None	
 
 def testParseLocalFile():
-        file = "./data/input_assign3.txt"
+        file = "./light_tester/data/input_assign3.txt"
         N, instructions = light_tester.LightTester.parseFile(file)
         assert N is not None
         assert instructions is not None
-
-#def test_length_parse():
-#    data = "10\nturn on 8,-40 through the pixels 9,11"
-#    z = re.split('\n', data)
-#    tv_screen = LightTester(z[0])
-#    assert tv_screen != None
-
-#def TXest_ten_pixels():
-#    data = "10\nturn on 8,-40 through the pixels 9,11"
-#    z = re.split('\n', data)
-#    for i in z:
-#        if z.index(i) != 0:
-#       		result = tv_screen.apply(i)
-#	else:
-#		pass		
-#    assert result == 20 
